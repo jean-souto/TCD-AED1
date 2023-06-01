cc4c4213d15dcd9c8c1ad8782af4aa48fe3d6a2e
Jean Souto <jeansouto123@gmail.com>
2023-06-01 04:52:01 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,21 +1,21 @@
 #define NULL_LIST 3
 #define EMPTY_LIST 2
 
-typedef struct plate // aqui
+typedef struct plateC // aqui
 { 
     char nome[40]; 
     char descricao[100]; 
     float valor; 
-} pratos; 
+} pratosC; 
 
-typedef struct ped // aqui
+typedef struct pedC // aqui
 { 
     int codigo; 
     float valorTotal; 
     char nome_rest[40]; 
-    pratos *ped; 
+    pratosC *ped; 
     int qtdPed; 
-} pedidos;
+} pedidosC;
 
 typedef struct card
 {
@@ -41,7 +41,7 @@ typedef struct cliente
     int codigo;
     cartao *pagamentos;
     int quantidade_cartoes;
-    pedidos *historico; // aqui
+    pedidosC *historico; // aqui
     int quant_pedidos;
     float valor_gasto;
     endereco *enderecos;
@@ -66,7 +66,7 @@ Lista_cliente *criarCliente (); // função criação
 int inserirInicioCliente (Lista_cliente *l, Cliente item); // insere no inicio da lista
 int inserirFimCliente (Lista_cliente *l, Cliente item); // insere no fim da lista
 int inserirCartaoCliente (Lista_cliente *l, int codigo, cartao novo_cartao);
-int inserirPedidoHistorico (Lista_cliente *l, int codigo, pedidos novo_pedido);
+int inserirPedidoHistorico (Lista_cliente *l, int codigo, pedidosC novo_pedido);
 int inserirEnderecoCliente (Lista_cliente *l, int codigo, endereco novo_end);
 
 // remoções
